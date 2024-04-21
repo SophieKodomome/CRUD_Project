48131172aae950c7feb2ee80837fe88bd025c695
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-04-21 17:45:14 +0300

    fix loading db

--- src/PSQLConnect.java
+++ src/PSQLConnect.java
@@ -6,7 +6,13 @@ public class PSQLConnect {
     private static String url = "jdbc:postgresql://localhost:5432/crud";
     private static String username = "crud";
     private static String password = "crud";
-
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
     }
